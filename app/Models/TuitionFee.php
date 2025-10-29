a8cf699deb9124c324620fee05601a4fc9f923ff
Farewellez <ahmadzafarell55@gmail.com>
2025-10-29 13:18:21 +0000

    feat(tuition-fees): Setup Model, Migration, Seeder, dan Form Request

--- app/Models/TuitionFee.php
+++ app/Models/TuitionFee.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\StudyProgram; # buat connect ke studyprogram
+use App\Models\Payment; # buat connect ke payment
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,11 +15,20 @@ class TuitionFee extends Model
     protected $table = 'tuition_fees';
 
     protected $fillable = [
-        'name',
+        'study_program_id', // ini buat fk dari program studi
+        'semester',
         'amount',
-        'description',
+        'payment_type',
+        'created_at',
+        'updated_at',
     ];
 
+    // biaya kuliah (1) : program studi (1)
+    public function studyProgram() {
+        return $this->belongsTo(StudyProgram::class);
+    }
+
+    // biaya kuliah (1) : payment (M)
     public function payments()
     {
         return $this->hasMany(Payment::class, 'tuition_fee_id');
