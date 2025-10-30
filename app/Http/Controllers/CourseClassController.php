f5cc4d822104fbfef29de0feb3a66de78edee52f
Nuhan-22 <email_github_nuhant379@gmail.com>
2025-10-30 23:49:16 +0700

    Add CourseClass model, migration, controller, seeder, and views

---
+++ app/Http/Controllers/CourseClassController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CourseClass;
+use App\Http\Requests\StoreCourseClassRequest;
+use App\Http\Requests\UpdateCourseClassRequest;
+
+class CourseClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseClassRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CourseClass $courseClass)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseClass $courseClass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseClassRequest $request, CourseClass $courseClass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseClass $courseClass)
+    {
+        //
+    }
+}
